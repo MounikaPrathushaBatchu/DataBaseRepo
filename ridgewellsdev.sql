d9d9964e05d6b79b7190c74a073988d0c834a0fb
MounikaPrathushaBatchu <122855938+MounikaPrathushaBatchu@users.noreply.github.com>
2023-07-28 14:27:38 +0530

    ridgewellsdev

---
+++ ridgewellsdev.sql
@@ -0,0 +1,6 @@
+select * from event_process_payment;
+select * from event;
+select * from xpertpayments_payment_options;
+select * from CardTypes;
+
+ select  * from reportlinks where reporttitle = 'Process Payment Receipt';
