5f53e92c2bc9bb5db8fee8711553e1a62eb8e7a8
MounikaPrathushaBatchu <122855938+MounikaPrathushaBatchu@users.noreply.github.com>
2023-08-09 18:02:14 +0530

    Add files via upload

--- loginCheck.sql
+++ loginCheck.sql
@@ -3,3 +3,5 @@ select * from user_session$;
 select * from userinfo;
 
 select count(us.id) as count from user_session$ us,userinfo u where u.loginid=us.loginid and us.session_id = 307841720 and  u.id =-1;
+
+select count(us.id) as count from user_session$ us,userinfo u where u.loginid=us.loginid and  u.id =-1;
